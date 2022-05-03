42ff6ab2f0e5fb00fdf8f6f485fc440d63a556a6
Dontcode44 <4659@holbertonschool.com>
2022-05-03 18:54:10 -0500

    Playing with the variables name

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,10 +6,10 @@ if number >= 0:
     lastdigi = number % 10
 else:
     tmp = number * -1
-    lastdigit = (tmp % 10) * -1
-if lastdigit > 5:
+    lastdigi = (tmp % 10) * -1
+if lastdigi > 5:
     print(f"Last digit of {number} is {lastdigi} and is greater than 5")
-elif lastdigit == 0:
+elif lastdigi == 0:
     print(f"Last digit of {number} is {lastdigi} and is 0")
 else:
     print(f'Last digit of {number} is {lastdigi} and is less than 6 and not 0')
