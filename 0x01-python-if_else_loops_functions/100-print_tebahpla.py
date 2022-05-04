e669c9bb9a45de49f4786a349c4bb8783f792681
Dontcode44 <4659@holbertonschool.com>
2022-05-04 00:27:16 -0500

    Try to do

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-for i in reversed(97, 123):
+for i in range(122, 96, -1):
     if i % 2 == 0:
-        j = chr(i)
+        tmp = chr(i)
     else:
-        j = chr(i-32)
-    print(f"{j}", end="")
+        tmp = chr(i - 32)
+        print(f"{tmp}", end="")
