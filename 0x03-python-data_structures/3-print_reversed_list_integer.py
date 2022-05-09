4e9324c4d15a0083deafd5e7c4860b3d9f379ddb
Dontcode44 <4659@holbertonschool.com>
2022-05-09 17:39:16 -0500

    Playing with the reverse

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-
-from audioop import reverse
 def print_reversed_list_integer(my_list=[]):
 
-    print(reverse(my_list))
+      for x in range(len(my_list)):
+          print(my_list[-1:])
