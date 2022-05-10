7ce61f906c58799953e8698b22c0f4f071c569a6
Dontcode44 <4659@holbertonschool.com>
2022-05-10 00:44:02 -0500

    Add tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    tupA = tuple_a (0, 0)
+    tupB = tuple_b (0, 0)
 
-    TuplA = tuple_a + (0, 0)
-    TuplB = tuple_b + (0, 0)
-    new_tuple = TuplA[0] + TuplB, TuplA[1] + TuplB[1]
-    return new_tuple
+    sum = tupA[0] + tupB[0] + tupA[1] + tupB[1]
+    return sum
