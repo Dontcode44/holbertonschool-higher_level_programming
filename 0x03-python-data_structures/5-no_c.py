d95d1079a32f584ae03cc6307badf565b1786d7f
Dontcode44 <4659@holbertonschool.com>
2022-05-09 20:01:25 -0500

    Try c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
-from re import I
-
-
 def no_c(my_string):
-    new_string = ""
 
+    new_string = ""
     for sr in my_string:
         if sr != "C" and sr != "c":
             new_string = new_string + sr
