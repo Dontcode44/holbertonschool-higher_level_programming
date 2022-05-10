815fe7a8cfaa7a7fdf186bb8d4f82d01e6ac5176
Dontcode44 <4659@holbertonschool.com>
2022-05-09 20:07:21 -0500

    Try c...

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 def no_c(my_string):
 
-    new_string = ""
-    for sr in my_string:
-        if sr != "C" and sr != "c":
-            new_string = new_string + sr
-    return new_string
+    new = ""
+    for strr in my_string:
+        if strr != "C" and strr != "c":
+            new = new + strr
+    return new
