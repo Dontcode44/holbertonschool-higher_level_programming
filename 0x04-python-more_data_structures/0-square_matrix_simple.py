16827ade99ced0298584031b598e34c22a229769
Dontcode44 <4659@holbertonschool.com>
2022-05-10 21:34:47 -0500

    Try to found another way to do

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix:
-        return[list(map(lambda x: x**2, i) for i in matrix)]
+    newm = []
+    for i in matrix:
+        newm2 = []
+        for x in i:
+            newm2.append(x**2)
+        newm.append(newm2)
+    return newm
