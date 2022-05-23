95e729259a0f53d2e51e72a2e0dbc93e79c781da
Dontcode44 <4659@holbertonschool.com>
2022-05-23 18:58:50 -0500

    The first commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+
+    try:
+        for i in range(0, x):
+            print(f"{my_list}", end="")
+            count += 1
+        print("")
+        return x
+    except IndexError:
+        print("")
+        return count
