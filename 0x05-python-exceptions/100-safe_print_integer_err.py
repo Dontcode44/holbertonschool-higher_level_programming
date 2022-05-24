66a3ac9f8521a6273c40a5329f7d92d62aa8ea8c
Dontcode44 <4659@holbertonschool.com>
2022-05-23 23:41:24 -0500

    100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
 
     try:
         print("{:d}".format(value))
         return True
-    except(ValueError, TypeError) as vt:
+    except (ValueError, TypeError) as vt:
         sys.stderr.write("Exception: {}\n".format(vt))
         return False
+    else:
+        print("{:s} is not an integer".format(vt))
