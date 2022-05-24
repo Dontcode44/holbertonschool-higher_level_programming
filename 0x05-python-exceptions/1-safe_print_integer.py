ffc653591bc57dfc7f1d0aa05622aecead3c5191
Dontcode44 <4659@holbertonschool.com>
2022-05-23 19:32:48 -0500

    Safe a print

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+from logging import exception
+
+
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except:
+    except exception:
         return False
