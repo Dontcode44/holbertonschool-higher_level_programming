da469cff6936f6c474a99182d1ac225f8ccefe18
Dontcode44 <4659@holbertonschool.com>
2022-05-23 19:02:32 -0500

    Added the position on my_list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
 
     try:
         for i in range(0, x):
-            print(f"{my_list}", end="")
+            print(f"{my_list[i]}", end="")
             count += 1
         print("")
         return x
