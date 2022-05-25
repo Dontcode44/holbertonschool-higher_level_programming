9c4e5b3602aab8d8a6d4397a111825d04aea2662
Dontcode44 <4659@holbertonschool.com>
2022-05-24 19:09:31 -0500

    Def area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Init of the program'''
+
+
+class Square:
+    '''Definitios'''
+    def __init__(self, size=0):
+        self.__size = size
+        '''Validations'''
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+            '''Less than 0'''
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+    def area(self):
+        '''Returns'''
+        return self.__size * self.__size
