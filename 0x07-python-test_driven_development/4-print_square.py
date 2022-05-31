6f95cefc7a773d363d3abce5486a09e24e5dd6d2
Dontcode44 <4659@holbertonschool.com>
2022-05-30 23:56:26 -0500

    Fixed many spaces to check

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Function that prints a square
+With a character '#'
+"""
+
+
+
+def print_square(size):
+    """Validations"""
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#", end="")
