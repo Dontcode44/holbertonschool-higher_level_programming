a7c0b074b73157a6b47aa6a75119845abf6624ee
Dontcode44 <4659@holbertonschool.com>
2022-06-01 02:46:02 -0500

    Try to do a for

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -13,4 +13,6 @@ def print_square(size):
         raise ValueError("size must be >= 0")
 
     for i in range(size):
-        print("#", end="")
+        for j in range(size):
+            print("#", end="")
+        print("")
