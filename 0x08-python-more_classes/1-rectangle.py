165bc967c91dfb18bef6295547cf01a97cd7d8a2
Dontcode44 <4659@holbertonschool.com>
2022-05-31 19:18:06 -0500

    Setters and getters

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,11 +1,38 @@
 #!/usr/bin/python3
-Rectangle = __import__('2-rectangle').Rectangle
 
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+'''This is my Rectangle class'''
 
-print("--")
 
-my_rectangle.width = 10
-my_rectangle.height = 3
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+class Rectangle:
+    '''Definitions'''
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+    
+    @property
+    def width(self):
+        '''Getter of width'''
+        return self.__width
+    
+    @property
+    def height(self):
+        '''Getter of height'''
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        '''Setter to width'''
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >=0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        '''Setter to height'''
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >=0")
+        self.__height = value
