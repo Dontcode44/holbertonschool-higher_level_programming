6a593388a21cd463911bda12bb4c19c142a09c21
Dontcode44 <4659@holbertonschool.com>
2022-06-01 02:20:01 -0500

    Try to do

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -9,8 +9,12 @@ def print_square(size):
     """Validations"""
     if type(size) is not int:
         raise TypeError("size must be an integer")
+    if type(size) == float and size < 0:
+        raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
+    if size == 0:
+        return None
 
-    for i in range(size):
+    for row in range(size):
         print("#", end="")
