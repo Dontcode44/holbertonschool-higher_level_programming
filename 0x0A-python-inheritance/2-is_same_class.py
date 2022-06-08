9c3db68dfb8626076250e4e1eeaf0e362fc8561e
Dontcode44 <4659@holbertonschool.com>
2022-06-07 22:56:48 -0500

    Type, no more

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def is_same_class(obj, a_class):
+    '''Function that returns True o False'''
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
