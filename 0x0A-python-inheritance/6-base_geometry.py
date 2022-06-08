4ea39ec729bd2a7b9c33c0968b4dc67ff1f7730f
Dontcode44 <4659@holbertonschool.com>
2022-06-07 23:38:48 -0500

    Adds the raise Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Initialization of the class'''
+
+
+class BaseGeometry:
+    '''Empty class'''
+def area(self):
+    '''Return a raiseError'''
+    raise Exception("area() is not implemented")
