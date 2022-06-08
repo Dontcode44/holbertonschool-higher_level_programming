3ad879a087421f00629bb4c86ea0f63217eb6efd
Dontcode44 <4659@holbertonschool.com>
2022-06-08 01:18:46 -0500

    Ussing supper

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -6,6 +6,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     '''Creation of the little class'''
     def __init__(self, width, height):
+        '''Using super to access'''
         super().integer_validation("width", width)
         super().integer_validation("height", height)
         self.__width = width
