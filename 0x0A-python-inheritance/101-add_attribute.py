50ecd9252ecf0ac08787b91d2e9099e8d55dbcbb
Dontcode44 <4659@holbertonschool.com>
2022-06-08 00:51:08 -0500

    Try

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,6 +5,7 @@
 def add_attribute(MyClass, attribute, value):
     '''Using the hasatrr'''
     if hasattr(MyClass, "__dict__"):
+        '''Setters the data'''
         setattr(MyClass, attribute, value)
     else:
         raise TypeError("can't add new atribute")
