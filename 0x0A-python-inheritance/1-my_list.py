8e123d8940118a48ca94bb7cc03057bf9220d49a
Dontcode44 <4659@holbertonschool.com>
2022-06-07 23:42:21 -0500

    My_list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-
-'''
-Class that inherits a list
-'''
+'''Class that inherits a list'''
 
 
 class MyList(list):
     '''My class'''
 
     def print_sorted(self):
-        """
-        Print list in sort order
-        """
+        """Print list in sort order"""
         print(sorted(self))
