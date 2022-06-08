d9c9d4f4ce09982713b7e871c8d1657461b63e6c
Dontcode44 <4659@holbertonschool.com>
2022-06-08 01:14:57 -0500

    Ussing supper

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Initialization of the function'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''Creation of the little class'''
+    def __init__(self, width, height):
+        super().integer_validation("width", width)
+        super().integer_validation("height", height)
+        self.__width = width
+        self.__height = height
