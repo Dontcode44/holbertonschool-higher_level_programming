27135cb301029832c3d0356799090655498f91c2
Dontcode44 <4659@holbertonschool.com>
2022-06-08 20:40:06 -0500

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 '''Initialization of the function'''
+
+
 def read_file(filename=""):
     '''Function that read a file in UTF8'''
-    with open(filename, 'r', encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
