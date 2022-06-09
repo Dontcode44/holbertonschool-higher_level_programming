804fc3bba37d537206aa28dbb7298ca4bab03bee
Dontcode44 <4659@holbertonschool.com>
2022-06-08 20:47:55 -0500

    Write file text

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Initialization of the class'''
+
+
+def write_file(filename="", text=""):
+    '''Function that returns the number of characters written'''
+    with open(filename, "w", encoding="utf-8") as f
+    return f.write(text)
