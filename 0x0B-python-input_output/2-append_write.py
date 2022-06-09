a715da0628c8bef4c461daa28d51fdf82aae243c
Dontcode44 <4659@holbertonschool.com>
2022-06-08 20:55:17 -0500

    Permissions to append

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Initialization of the class'''
+
+
+def append_write(filename="", text=""):
+    '''Appends a string and returns number of char adds'''
+    with open(filename, "a", encoding="utf-8") as filexd
+    return filexd.write(text)
