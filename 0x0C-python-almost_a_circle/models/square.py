afd7b17f2210d3e4df5d78f043a3cc53e72f52de
Dontcode44 <4659@holbertonschool.com>
2022-06-14 16:31:54 -0500

    Class, getters, setters and __str__

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Children class, Square'''
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    '''Square inherits from Rectangle'''
+    def __init__(self, size, x=0, y=0, id=None):
+        '''Class constructor'''
+        super().__init__(id, x, y, size, size)
+
+    @property
+    def size(self):
+        return self.width
+    
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+    
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+
