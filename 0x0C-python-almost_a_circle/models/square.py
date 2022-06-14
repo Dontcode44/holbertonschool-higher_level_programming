e7e506a9a4c710824830359d4f238d1357731357
Dontcode44 <4659@holbertonschool.com>
2022-06-14 17:04:28 -0500

    Fixed - Spaces from pycode

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,4 +25,21 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         '''Update Square'''
-        if 
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.id = args[1]
+        if len(args) >= 3:
+            self.id = args[2]
+        if len(args) >= 4:
+            self.id = args[3]
+        if len(args) == 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.width = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
