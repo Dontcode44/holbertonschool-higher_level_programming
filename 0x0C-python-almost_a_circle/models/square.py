a2a5ed343830109aad9abbe3308a40168fc02926
Dontcode44 <4659@holbertonschool.com>
2022-06-14 16:38:22 -0500

    Fixed - Many spaces and indentation from pycode

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,12 +12,12 @@ class Square(Rectangle):
     @property
     def size(self):
         return self.width
-    
+
     @size.setter
     def size(self, value):
         self.width = value
         self.height = value
-    
-    def __str__(self):
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
 
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
+                                                 self.y, self.width)
