a50cc6275ae8d7e20a2445c3fad8a0ab5f46658d
Dontcode44 <4659@holbertonschool.com>
2022-06-14 14:23:38 -0500

    Fixed many spaces from pycode

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,6 +4,9 @@ from models.base import Base
 
 
 class Rectangle(Base):
+    '''Child class'''
+
+
     def __init__(self, width, height, x=0, y=0, id=None):
         '''Child class'''
         super().__init__(id)
