c7d063e5cdd8541ca211b2416b8c41705baaf131
Dontcode44 <4659@holbertonschool.com>
2022-06-14 17:25:50 -0500

    Dictionary

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -47,4 +47,4 @@ class Square(Rectangle):
 
     def to_dictionary(self):
         '''Returns the dictionary representation'''
-        return {'x': self.x, 'y': self.y, 'id': self.id, 'size': self.width}
+        return {'id': self.id, 'width': self.width, 'x': self.x, 'y': self.y}
