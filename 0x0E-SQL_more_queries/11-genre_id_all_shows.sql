2066e6ff1cb9839a0dcc80357622dd6e1abe1d8b
Dontcode44 <4659@holbertonschool.com>
2022-07-14 23:59:41 -0500

    Genre

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- Write a script that lists all shows contained in the database hbtn_0d_tvshows.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows AS ts
-LEFT JOIN tv_show_genres ON ts.id = tv_shows_genres.show_id
-ORDER BY ts.title, tv_show_genres.genre_id;
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+ORDER BY ts.title, tv_show_genres.genre_id ASC;
