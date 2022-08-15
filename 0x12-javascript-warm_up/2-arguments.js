6ffda5067be0dff2c3b9623d99e927aace75e5c1
Dontcode44 <4659@holbertonschool.com>
2022-08-15 18:45:43 -0500

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,7 +4,7 @@ const args = process.argv.length;
 
 if (args == 2)
     console.log('No argument')
-if (args == 3)
+elif (args == 3)
     console.log('Argument found')
 else
     console.log('Arguments found')
