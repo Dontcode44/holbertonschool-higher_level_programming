609a77674dd2fec47af79d52c406d77d4ae18cc3
Dontcode44 <4659@holbertonschool.com>
2022-08-16 13:58:31 -0500

    Change the constructor for this

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -9,8 +9,8 @@ module.exports = class Rectangle {
   }
 
   print () {
-    for (let index = 0; index < this.width; index++) {
-      console.log('X'.repeat(this.width));
+    for (let index = 0; index < this.height; index++) {
+      console.log('X'.repeat(this.height));
     }
   }
 };
