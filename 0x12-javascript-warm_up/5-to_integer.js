3e1fa39b407a92562edfd6595959ca863e94d86e
Dontcode44 <4659@holbertonschool.com>
2022-08-15 20:37:55 -0500

    Fixed - Parse in a var

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 
-if (Number.isNaN(process.argv[2])) {
+const pars = parseInt(process.argv[2]);
+if (!pars) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + parseInt(process.argv[2]));
