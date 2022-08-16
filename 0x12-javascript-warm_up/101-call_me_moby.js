8037fc89f00d1a68912e2576306ae627b28252b2
Dontcode44 <4659@holbertonschool.com>
2022-08-15 22:30:42 -0500

    Advanced 101

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-exports.addMeMaybe = function (x, theFunction) {
+exports.callMeMoby = function (x, theFunction) {
   for (let i = 0; i < x; i++) {
-    theFunction.call();
+    theFunction();
   }
 };
