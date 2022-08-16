6d51f000d03127ffe718af49ea9e1f241d2b4cdd
Dontcode44 <4659@holbertonschool.com>
2022-08-16 17:48:44 -0500

    Fixed - Many spaces from semistandard

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,7 +5,8 @@ module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
-  charPrint(c) {
+
+  charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
@@ -14,4 +15,3 @@ module.exports = class Square extends Rectangle {
     }
   }
 };
-
