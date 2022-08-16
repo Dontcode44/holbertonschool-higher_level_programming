91dd18a40e3e415da466747c7591640fcb4fdb8d
Dontcode44 <4659@holbertonschool.com>
2022-08-16 18:27:47 -0500

    Square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-const Rectangle = require('./5-square');
+const Rectan = require('./5-square');
 
-class Square extends Rectangle {
+module.exports = class Square extends Rectan {
   charPrint (c) {
     if (c === undefined) { c = 'X'; }
     for (let index = 0; index < this.height; index++) {
       console.log(c.repeat(this.width));
     }
   }
-}
-module.exports = Square;
+};
