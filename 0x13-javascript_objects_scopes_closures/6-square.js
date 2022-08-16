e17e3ed1d6c8323399ccd8882796f47ae7406b8e
Dontcode44 <4659@holbertonschool.com>
2022-08-16 18:22:59 -0500

    Fixed - Many spaces and indent and extra lines from semistandard

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 const Rectangl = require('./5-square');
 
-module.exports = class Square extends Rectangl {
+class Square extends Rectangl {
   charPrint (c) {
     if (c === undefined) { c = 'X'; }
     for (let index = 0; index < this.height; index++) {
       console.log(c.repeat(this.width));
     }
   }
-};
+}
+module.exports = Square;
