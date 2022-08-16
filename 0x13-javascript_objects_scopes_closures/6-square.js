b848b99050456a242bb33acb6c254260636e4f84
Dontcode44 <4659@holbertonschool.com>
2022-08-16 18:00:48 -0500

    Fixed - Many spaces and indent from semistandard

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,9 +7,7 @@ module.exports = class Square extends Rectangle {
   }
 
   charPrint (c) {
-    if (c === undefined) {
-      c = 'X';
-    }
+    if (c === undefined) { c = 'X'; }
     for (let index = 0; index < this.height; index++) {
       console.log('c'.repeat(this.width));
     }
