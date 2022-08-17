b5ec7114a90ebd0ea80fb550baf94e850858b113
Dontcode44 <4659@holbertonschool.com>
2022-08-16 23:19:43 -0500

    Esver

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@
 exports.esrever = function (list) {
   const revr = new Array;
   for (var i = list.length - 1; i >= 0; i--) {
-    revr.push (list[i]);
+    revr.push(list[i]);
   }
   return revr;
 };
