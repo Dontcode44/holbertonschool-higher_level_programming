91fa47686d7e190c0843ce1ff9c903bc13c0977a
Dontcode44 <4659@holbertonschool.com>
2022-08-17 21:27:45 -0500

    Request

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const fs = require('fs');
 
-request(process.argv[2], function (error, response, body) {
+request(process.argv[2], function (err, response, body) {
   fs.writeFile(process.argv[3], body, 'utf-8', function (err) {
     if (err) {
       console.log(err);
