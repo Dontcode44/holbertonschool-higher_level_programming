515a002bac8537f9bdf76efdfcd1dfa420955359
Dontcode44 <kabutto378@gmail.com>
2022-08-30 23:41:14 -0500

    Fix - Pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,6 @@ import MySQLdb
 import sys
 if __name__ == "__main__":
 
-
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=sys.argv[1], passwd=sys.argv[2],
                            db=sys.argv[3])
@@ -15,5 +14,5 @@ if __name__ == "__main__":
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
-cur.close()
-conn.close()
+    cur.close()
+    conn.close()
