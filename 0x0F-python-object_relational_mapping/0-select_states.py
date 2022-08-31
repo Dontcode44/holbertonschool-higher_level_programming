a96eeeb7670688893885127672d37abff06939c8
Dontcode44 <kabutto378@gmail.com>
2022-08-30 23:22:47 -0500

    Select all states and Readme

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+import MySQLdb
+import sys
+if __name__ == "__main__":
+
+    conn = MySQLdb.connect(host="localhost",
+                        port=3306,
+                        user=sys.argv[0],
+                        passwd=sys.argv[1],
+                        db=sys.argv[2],
+                        charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
