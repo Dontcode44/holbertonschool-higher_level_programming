2490dfa6f1caec96e8f014e5dfe52f4a5bb06a7e
Dontcode44 <kabutto378@gmail.com>
2022-09-02 00:00:42 -0500

    Model with sessionmaker

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -15,5 +15,5 @@ if __name__ == '__main__':
     session = Session()
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
-        
+
     session.close()
