d921cb917a194eeea37e4a6f9f04ab14409da361
Dontcode44 <kabutto378@gmail.com>
2022-09-05 00:15:01 -0500

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL, sends a POST
-curl -sX POST --data "$1" 'email=test@gmail.com' --data 'subject=I will always be here for PLD'
+curl -sX POST --data "$1" 'email=test@gmail.com&subject=I will always be here for PLD'
