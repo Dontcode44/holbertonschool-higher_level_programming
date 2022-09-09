3dbe31b00ca62da7ad125f260eb33d8cf64569be
Dontcode44 <kabutto378@gmail.com>
2022-09-09 15:12:12 -0500

    Task 0 get

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,9 +2,9 @@
 # Write a Python script that fetches https://intranet.hbtn.io/status
 import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    re = res.read()
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+    doc = res.read()
     print("Body response:")
-    print("\t- type: {}".format(type(re)))
-    print("\t- content: {}".format(re))
-    print("\t- utf8 content: {}".format(re.decode("utf-8")))
+    print("\t- type: {}".format(type(doc)))
+    print("\t- content: {}".format(doc))
+    print("\t- utf8 content: {}".format(doc.decode("utf-8")))
