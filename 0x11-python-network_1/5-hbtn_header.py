16d668e00c973e41edfde91ae10b77906a04dc7c
Dontcode44 <kabutto378@gmail.com>
2022-09-09 16:06:20 -0500

    Add - Request with request

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,6 +5,7 @@ Write a Python script that takes in a URL, sends a request to the URL and displa
 import requests
 import sys
 
-url = sys.argv[1]
-response = requests.get(url)
-print(response.headers.get('X-Request-Id'))
+if __name__ == '__main__':
+    url = sys.argv[1]
+    response = requests.get(url)
+    print(response.headers.get('X-Request-Id'))
