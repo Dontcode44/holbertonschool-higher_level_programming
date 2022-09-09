cf68d7144c73e595e3be963c6d3af6610e55d705
Dontcode44 <kabutto378@gmail.com>
2022-09-09 17:26:40 -0500

    Github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,6 +9,6 @@ import sys
 
 if __name__ == "__main__":
     basic = requests.auth.HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    url = requests.get("https://api.github.com/user", request.auth=basic)
+    url = requests.get("https://api.github.com/user", auth=basic)
     status, id_ = url.status_code, url.json().get("id")
     print('None') if status >= 400 else print(id_)
